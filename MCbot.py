3d0a8dfd6e8a55b561d12731bc5ac5c7c7f025da
Oxidiz3 <mecham.porter@gmail.com>
2020-11-29 17:54:45 -0700

    exracted the start server command to it's own function

--- MCbot.py
+++ MCbot.py
@@ -2,19 +2,14 @@ import os
 import discord
 import psutil
 import subprocess
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 def checkIfProcessRunning(processName):
-    '''
-    Check if there is any running process that contains the given name processName.
-    '''
-    #Iterate over the all the running process
+    # Iterate over the all the running process
     for proc in psutil.process_iter():
         try:
             # Check if process name contains the given name string.
@@ -22,65 +17,72 @@ def checkIfProcessRunning(processName):
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    return False;
+    return False
 
 def serverRunning():
-	if(checkIfProcessRunning('java.exe')):
-		return True
-	else:
-		return False
+    if(checkIfProcessRunning('java.exe')):
+        return True
+    else:
+        return False
 
+def startServer():
+    os.chdir("C:/Users/Lynn/Desktop/Server")
+    process = subprocess.Popen(
+        "startup.bat", shell=True, stdin=subprocess.PIPE)
+
+# ===Discord Events===
 @client.event
-#run when client is ready
 async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-            return
-    
+        return
+
     if message.content == "/start":
-            if(serverRunning()):
-                    await message.channel.send("Server is already running")
-            else:
-                    await message.channel.send("Starting server")
-                    os.chdir("C:/Users/Lynn/Desktop/Server")
-                    subprocess.Popen("startup.bat", shell=True)
+        if(serverRunning()):
+            await message.channel.send("Server is already running")
+        else:
+            await message.channel.send("Starting server")
+            startServer()
     elif message.content == "/stop":
-            if(serverRunning()):
-                    await message.channel.send("Closing Server")
-            else:
-                    await message.channel.send("Server isn't running")
+        if(serverRunning()):
+            await message.channel.send("Closing Server")
+        else:
+            await message.channel.send("Server isn't running")
 
 client.run(TOKEN)
+
+
 ##
 ##
 ##process_id = 0
-##def on_message(message):
-##    if message.content == "/start":
-##        
+# def on_message(message):
+# if message.content == "/start":
+##
 ##        process_id = start()
-##        if process_id > 0 && process_id not == False:
-##            return true
-##        else:
-##            return false
-##    elif message.content == "/stop":
+# if process_id > 0 && process_id not == False:
+# return true
+# else:
+# return false
+# elif message.content == "/stop":
 ##       result = stop(process_id)
-##    else:
-##        return "Server isn't started yet"
+# else:
+# return "Server isn't started yet"
 ##
-##def start():
-##    #Start procedures
-##    return sb.process_id();
+# def start():
+# Start procedures
+# return sb.process_id();
 ##
 ##
-##def stop(process_id):
-##    #Send stop command to process using the process id
-##    if not serverRunning():
+# def stop(process_id):
+# Send stop command to process using the process id
+# if not serverRunning():
 ##        process_id = 0
-##        return true
-##    else:
+# return true
+# else:
 ##        print("ERROR ERROR SERVER IS NOT RESPONDING")
-##        return false
-##    
+# return false
+##
