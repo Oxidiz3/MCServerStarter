f5a220a110a2123126d43fe9fd63fe2ada531263
canman333 <mecham.lynn@gmail.com>
2020-12-18 15:32:03 -0700

    more updates

--- MCbot.py
+++ MCbot.py
@@ -1,6 +1,5 @@
 import os
 # import discord
-import psutil
 # import subprocess
 from dotenv import load_dotenv
 from process_class import Process
@@ -9,22 +8,22 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 # client = discord.Client()
 
-def checkIfProcessRunning(processName):
-    # Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False
+# def checkIfProcessRunning(processName):
+#     # Iterate over the all the running process
+#     for proc in psutil.process_iter():
+#         try:
+#             # Check if process name contains the given name string.
+#             if processName.lower() in proc.name().lower():
+#                 return True
+#         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+#             pass
+#     return False
 
-def serverRunning():
-    if checkIfProcessRunning('java.exe'):
-        return True
-    else:
-        return False
+# def serverRunning():
+#     if checkIfProcessRunning('java.exe'):
+#         return True
+#     else:
+#         return False
 
 process = Process(TOKEN)
 
