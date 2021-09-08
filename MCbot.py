83880f9509e5a2a14f60fc9d5460d7a16614c82c
Oxidiz3 <mecham.porter@gmail.com>
2021-09-08 13:25:34 -0600

    added a class manager

--- MCbot.py
+++ MCbot.py
@@ -4,43 +4,52 @@ import psutil
 import subprocess
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-process = None
 
-def checkIfProcessRunning(processName):
-    # Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False
+class ServerManager:
+    def __init__(self):
+        load_dotenv()
+        self.TOKEN = os.getenv("DISCORD_TOKEN")
+        self.client = discord.Client()
+        self.process = None
 
-def serverRunning():
-    if(checkIfProcessRunning('java.exe')):
-        return True
-    else:
+    def serverRunning(self):
+        if self.checkIfProcessRunning("java.exe"):
+            return True
+        else:
+            return False
+
+    def startServer(self):
+        os.chdir("/home/smecham/ForgeServer")
+        self.process = subprocess.Popen(
+            "/home/smecham/ForgeServer/start.sh",
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    def close_server(self):
+        self.process.communicate("stop")
+        print("CLOSING SERVER =======================")
+
+    def checkIfProcessRunning(self, processName):
+        # Iterate over the all the running process
+        for proc in psutil.process_iter():
+            try:
+                # Check if process name contains the given name string.
+                if processName.lower() in proc.name().lower():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
         return False
 
-def startServer():
-    global process
-    os.chdir("/home/smecham/ForgeServer")
-    process = subprocess.Popen(
-        "/home/smecham/ForgeServer/start.sh", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-def close_server():
-    global process
-    process.communicate("stop")
-    print("CLOSING SERVER =======================")
+server_manager = ServerManager()
 
 # ===Discord Events===
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{client.user} has connected to Discord!")
 
 
 @client.event
@@ -49,17 +58,17 @@ async def on_message(message):
         return
 
     if message.content == "/start":
-        if(serverRunning()):
+        if server_manager.serverRunning():
             await message.channel.send("Server is already running")
         else:
             await message.channel.send("Starting server")
-            startServer()
+            server_manager.startServer()
     elif message.content == "/stop":
-        if(serverRunning()):
+        if server_manager.serverRunning():
             await message.channel.send("Closing Server")
-            close_server()
+            server_manager.close_server()
         else:
             await message.channel.send("Server isn't running")
 
-client.run(TOKEN)
 
+client.run(server_manager.TOKEN)
